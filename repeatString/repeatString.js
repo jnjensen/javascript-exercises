5597c123d73e446e5a96d3c73508b3ed448f6f56
James <jnjensen@live.com>
2020-05-25 22:08:02 -0500

    Finished helloWorld, repeatString, & reverseString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
-}
+const repeatString = function(word, num) {
+    var newWord = ""
+    if (num >= 0){
+        for (i = 0; i < num; i++){
+            newWord += word;
+        }
+    } else {
+        return "ERROR";
+    }
+    return newWord;
+    }
 
 module.exports = repeatString
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(words) {
+    var newString = "";
+    var num = (words.length - 1)
 
+    for (i = 0; i <= num; i++){
+        newString += words[(num - i)];
+    }
+    return newString;
 }
 
 module.exports = reverseString
