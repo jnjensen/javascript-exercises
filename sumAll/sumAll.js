0c8de8ff550973b946b48051d2a6c077c06e0641
James <jnjensen@live.com>
2020-05-27 21:45:31 -0500

    Resovled sumAll.

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    var total = 0;
+    var x = typeof num1;
+    var y = typeof num2;
 
+    if (x == "number" && y == "number" && num1 >= 0 && num2 >= 0){
+        if (num1 < num2){
+            for (i = num1; i <= num2; i++){
+                total += i;
+            }
+        } else {
+            for (i = num2; i <= num1; i++){
+                total += i;
+            }
+        }
+        return total;
+    } 
+    return "ERROR";
 }
 
 module.exports = sumAll
