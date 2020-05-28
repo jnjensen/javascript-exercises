167aa80a8f450fef94ffc9d99416a6652b98dcda
James <jnjensen@live.com>
2020-05-27 21:56:22 -0500

    Resolved leapYears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    var four = year % 4;
+    var hundred = year % 100;
+    var fourHundred = year % 400;
 
+    if (four == 0 && hundred != 0){
+        return true;
+    } else if (four == 0 && hundred == 0){
+        if (fourHundred == 0){
+            return true;
+        }
+    } 
+
+    return false;
 }
 
 module.exports = leapYears
