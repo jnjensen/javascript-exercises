9108b471371466d25ed2d0e56049657684ba1af7
James <jnjensen@live.com>
2020-05-27 22:14:12 -0500

    Resolved tempConversion.

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
+const ftoc = function(far) {
+  var celsius = ((far - 32) * (5/9)).toFixed(1);
+  
+  if (celsius[3] == 0){
+    celsius.toFixed(0);
+  }
 
+  return Number(celsius);
 }
 
-const ctof = function() {
+const ctof = function(celsius) {
+  var far = ((celsius * (9/5)) + 32).toFixed(1);
 
+  return Number(far);
 }
 
 module.exports = {
