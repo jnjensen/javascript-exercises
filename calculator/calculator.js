f8b1131fd8c5b9e9e4e8c4c1a6512953e3ae5598
James <jnjensen@live.com>
2020-06-06 21:28:46 -0500

    completed Calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,57 @@
-function add () {
-	
+function add (num1, num2) {
+	return (num1 + num2);
 }
 
-function subtract () {
-	
+function subtract (num1, num2) {
+	return (num1 - num2);
 }
 
-function sum () {
-	
+function sum (array) {
+	if (array.length == 0){
+		return 0;
+	}else{
+		var sum = 0;
+		for (i = 0; i < array.length; i++){
+			sum = (sum + array[i]);
+		}
+		return sum;
+	}
 }
 
-function multiply () {
-	
+function multiply (array) {
+	if (array.length == 0){
+		return 0;
+	}else{
+		var product = 1;
+		for (i = 0; i < array.length; i++){
+			product = (product * array[i]);
+		}
+		return product;
+	}
 }
 
-function power() {
-	
+function power(num, pow) {
+	if (pow == 0){
+		return 0;
+	}else{
+		var total = num;
+		for (i = 1; i < pow; i++){
+			total = (total * num);
+		}
+		return total;
+	}
 }
 
-function factorial() {
-	
+function factorial(num) {
+	if (num == 0){
+		return 1;
+	} else{
+		var total = 1;
+		for (i = 1; i <= num; i++){
+			total = (total * i);
+		}
+		return total;
+	}
 }
 
 module.exports = {
