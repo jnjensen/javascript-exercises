d4eca7f1c95783c918c36cec4eb7d9591cf2d9a6
James <jnjensen@live.com>
2020-06-07 21:02:23 -0500

    Fibonacci challenge completed

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(pos) {
+    var idx = (Number(pos) - 1);
+    var fib = [1, 1];
 
+    if (pos <= 0){
+        return "OOPS";
+    } else if (pos == 1 || pos == 2){
+        return 1;
+    } else {
+        for (i = 1; i < idx; i++){
+            var sum = (fib[(i-1)] + fib[i]);
+            fib.push(sum);
+        }
+    }
+    return fib[idx];
 }
 
 module.exports = fibonacci
